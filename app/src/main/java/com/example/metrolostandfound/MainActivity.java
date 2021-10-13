4b756c723153bd3e51673cc6a0f5edaa906f11dd
won31080 <won31080@gmail.com>
2021-10-13 23:47:39 +0900

    프로젝트 시작

---
+++ app/src/main/java/com/example/metrolostandfound/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.metrolostandfound;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnGoSearch = (Button) findViewById(R.id.btnGoSearch);
+
+        btnGoSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+                onStop();
+            }
+        });
+    }
+
+    //뒤로가기 두번 누르면 종료
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+    final String PREFNAME = "Preferences";
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            SharedPreferences settings = getSharedPreferences(PREFNAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = settings.edit();
+
+            editor.putBoolean("Appexec", true);
+            editor.apply();
+            ActivityCompat.finishAffinity(this);
+            toast.cancel();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/metrolostandfound/SearchActivity.java
@@ -0,0 +1,34 @@
+package com.example.metrolostandfound;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "검색", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        onStop();
+    }
+}
