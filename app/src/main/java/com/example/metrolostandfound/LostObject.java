f202da82f9162dc7528bce31e32c13354bca0249
won31080 <won31080@gmail.com>
2021-11-01 21:43:10 +0900

    이미지 저장 일단 Bitmap - String 변환으로 구현해봄. 근데 이건 앱에서 데이터 전송을 먼저 해야 확인가능

--- app/src/main/java/com/example/metrolostandfound/LostObject.java
+++ app/src/main/java/com/example/metrolostandfound/LostObject.java
@@ -1,6 +1,10 @@
 package com.example.metrolostandfound;
 
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 //DB에서 파일 불러온거 담기 위한 클래스
 //다 String인건 어쩔 수 없는듯 다 따로 Text나 Date로 변환하는 과정 필요
@@ -18,6 +22,7 @@ public class LostObject
     private String contents;
     private String station;
     private String main_category;
+    private String image;
 
     //얘들은 DB 등록될 때 자동으로 만들어지는 정보
     private String id;
@@ -25,7 +30,12 @@ public class LostObject
     private String updated_at;
     private String created_at;
     
-    //image는 json에서 변환해오는 과정에 문제가 있어서 다른 방법을 좀 찾아봐야함
+    //10/31 image는 json에서 변환해오는 과정에 문제가 있어서 다른 방법을 좀 찾아봐야함
+    //11/01 원래 이미지만 저장하는 테이블 만들어서 따로 받아오면 어떨까 했는데 
+    //      생각해보니까 데이터에 해당하는 이미지 불러오려면 id까지 저장해야 해서 똑같은 오류가 발생할 기분
+    //      그래서 그냥 String으로 저장하고 Bitmap - String간 변환을 통해 해보면 어떨까 해서 그렇게 해봄
+    //      사용하는 사람은 그냥 Bitmap 이미지를 쓴다고 생각하고 쓰면 됨 변환 알아서 다 됨
+    //      근데 이건 DB에서 임의로 올릴 수 있는게 아니라 나중에 등록 구현하고 앱에서 등록해봐야함
 
     public String getItemId () { return item_id; }
     public void setItem_id (String item_id) { this.item_id = item_id; }
@@ -51,10 +61,14 @@ public class LostObject
     public String getMainCategory () { return main_category; }
     public void setMainCategory (String main_category) { this.main_category = main_category; }
 
+    //String으로 이미지 데이터 관리 - retrofit 라이브러리가 이미지를 받아오는 데에 문제가 있어서
+    public Bitmap getImage(){ return BitmapConverter.StringToBitmap(image); }
+    public void setImage(Bitmap img){ image = BitmapConverter.BitmapToString(img); }
+
     @Override
     public String toString()
     {
-        return "LostObject [item_id = "+item_id+", sub_category = "+sub_category+", line = "+line+", created_at = "+created_at+", storage = "+storage+", date_time = "+date_time+", updated_at = "+updated_at+", contents = "+contents+", station = "+station+", main_category = "+main_category+", id = "+id+", published_at = "+published_at+"]";
+        return "LostObject [item_id = "+item_id+", sub_category = "+sub_category+", line = "+line+", created_at = "+created_at+", storage = "+storage+", date_time = "+date_time+", updated_at = "+updated_at+", contents = "+contents+", station = "+station+", main_category = "+main_category+", id = "+id+", published_at = "+published_at+", image = "+image+"]";
     }
 
     //이 밑의 항목들은 따로 지정해주지 않아도 DB에서 알아서 지정, 읽을 때만 있으면 됨
@@ -66,4 +80,42 @@ public class LostObject
     private void setCreated_at (String created_at) { this.created_at = created_at; }
     private String getUpdated_at () { return updated_at; }
     private void setUpdated_at (String updated_at) { this.updated_at = updated_at; }
+}
+
+// 비트맵과 스트링 간의 변환을 위한 클래스
+// 출처 : https://youngest-programming.tistory.com/95
+class BitmapConverter {
+    /*
+     * String형을 BitMap으로 변환시켜주는 함수
+     * */
+    public static Bitmap StringToBitmap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+    /*
+     * Bitmap을 String형으로 변환
+     * */
+    public static String BitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 70, baos);
+        byte[] bytes = baos.toByteArray();
+        String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return temp;
+    }
+
+    /*
+     * Bitmap을 byte배열로 변환
+     * */
+    public static byte[] BitmapToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+        return baos.toByteArray();
+    }
 }
