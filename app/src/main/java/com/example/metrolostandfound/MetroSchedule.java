3ac48e26dd0d1a14408104f045160bc2700997c0
won31080 <won31080@gmail.com>
2021-11-24 06:21:26 +0900

    열차별로 시간표 하는거 일단 만들어본거

--- app/src/main/java/com/example/metrolostandfound/MetroSchedule.java
+++ app/src/main/java/com/example/metrolostandfound/MetroSchedule.java
@@ -15,7 +15,9 @@ import jxl.Workbook;
 
 public class MetroSchedule {
     private static Map<String, Map<String, List<String>>> suinbundang;
+    private static Map<String, List<String>> suinbundangTrainTimes;
     private static List<String> suinbundangStationList;
+    private static List<String> suinbundangTrainList;
     //순서를 맞추기 위해
     private static List<String> lines;
 
@@ -49,15 +51,14 @@ public class MetroSchedule {
                     for (int row = rowIndexStart; row < rowTotal; row++) {
                         List<String> times = new ArrayList<>();
                         for (int col = 1; col < colTotal; col++) {
-                            if(col == 1){
+                            if (col == 1) {
                                 String contents = sheet.getCell(col, row).getContents();
                                 contents = contents.replaceAll(" ", "");
-                                if(contents.length() != 0){
+                                if (contents.length() != 0) {
                                     name = contents;
                                     suinbundangStationList.add(contents);
                                 }
-                            }
-                            else {
+                            } else {
                                 String contents = sheet.getCell(col, row).getContents();
                                 contents = contents.replaceAll(" ", "");
                                 if (contents.length() != 0) {
@@ -65,15 +66,53 @@ public class MetroSchedule {
                                 }
                             }
                         }
-                        if(row%2 == 1){ //홀수 인덱스에는 도착시간 (역에 도착하는 시간)
+                        if (row % 2 == 1) { //홀수 인덱스에는 도착시간 (역에 도착하는 시간)
                             Map<String, List<String>> d = new HashMap<String, List<String>>();
                             d.put("도착시간", times);
                             suinbundang.put(name, d);
-                        }
-                        else{ //짝수 인덱스에는 출발시간 (역에서 출발하느 시간)
+                        } else { //짝수 인덱스에는 출발시간 (역에서 출발하느 시간)
                             Map<String, List<String>> d = new HashMap<String, List<String>>();
                             d.put("출발시간", times);
                             suinbundang.put(name, d);
+                        }
+                    }
+                }
+            }
+            if (wb != null) {
+                Sheet sheet = wb.getSheet(0);
+                if (sheet != null) {
+                    int colTotal = sheet.getColumns();
+                    int rowIndexStart = 2;
+                    int rowTotal = sheet.getColumn(0).length;
+                    for (int i = 1; i < colTotal; i++) {
+                        if (rowTotal < sheet.getColumn(i).length)
+                            rowTotal = sheet.getColumn(i).length;
+                    }
+
+                    String name = null;
+
+                    for (int col = 2; col < colTotal; col++) {
+                        List<String> times = new ArrayList<>();
+                        name = "";
+                        for (int row = rowIndexStart; row < rowTotal; row++) {
+                            if (row == rowIndexStart) {
+                                String contents = sheet.getCell(col, row).getContents();
+                                contents = contents.replaceAll(" ", "");
+                                if (contents.length() != 0) {
+                                    name = contents;
+                                    suinbundangTrainList.add(contents);
+                                }
+                            } else {
+                                String contents = sheet.getCell(col, row).getContents();
+                                contents = contents.replaceAll(" ", "");
+                                if (contents.length() != 0) {
+                                    times.add(contents);
+                                }
+                            }
+                        }
+                        if (!name.equals("")) { //홀수 인덱스에는 도착시간 (역에 도착하는 시간)
+                            suinbundangTrainTimes.put(name, times);
+                            Log.d("열차별시간", name + times.toString());
                         }
                     }
                 }
